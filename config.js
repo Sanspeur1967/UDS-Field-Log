3f80997a1bc7e920b3dfbd0e992bb3e8c1dec617
Sanspeur1967 <allansutherland4@msn.com>
2026-06-28 05:35:14 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
-// UDS Development Log v4.3 config
 const SUPABASE_URL = "https://faqoholrqxsebkserkau.supabase.co";
-const SUPABASE_ANON_KEY = " sb_publishable_bRx5sKf9_n2rcQFFIWGkHA_S-0Ts8d6";
+const SUPABASE_ANON_KEY = "sb_publishable_bRx5sKf9_n2rcQFFIWGkHA_S-0Ts8d6";
 const PHOTO_BUCKET = "field-photos";
 const AI_FUNCTION_NAME = "UDS-AI";
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
