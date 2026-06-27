b71234b3da6fec6c929c05a07744b0b7798e075d
Sanspeur1967 <allansutherland4@msn.com>
2026-06-27 14:26:31 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,20 @@
-// Paste your Supabase values here later.
+// UDS Development Log v2 config
+// Paste your Supabase publishable/anon key below.
 // Do NOT paste the service_role or secret key.
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+
+const SUPABASE_URL = "https://faqoholrqxsebkserkau.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_bRx5sKf9_n2rcQFFIWGkHA_S-0Ts8d6";
+const PHOTO_BUCKET = "field-photos";
+
+
+
+
+
+
+
+
+
+
+
+
+
