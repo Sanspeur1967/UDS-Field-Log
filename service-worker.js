a4ee8832f12f7b96d9e7d9a15547bf4519af0857
Sanspeur1967 <allansutherland4@msn.com>
2026-06-28 16:45:05 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='uds-development-pro-enterprise-3-5';
+const CACHE='uds-development-pro-enterprise-4-0-rc1';
 const FILES=['./','./index.html','./style.css','./app.js','./config.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
